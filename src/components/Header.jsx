3d7abf9dd05aa8c4f61ada8f35ddd3232162e06e
Samuele Minissale <minissalesamuele@gmail.com>
2024-01-21 15:38:03 +0100

    fix header design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,23 @@ import { Github, BookUser } from "lucide-react"
 export default function Header() {
     return (
         <header className="header sticky top-0 z-30 backdrop-blur bg-surface/25 text-primary shadow">
-            <img 
-                src=""
-                className="header--image"
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <nav className="flex flex-row gap-1">
-                <ThemeSwitcher />
-                <a href="https://github.com/SamPwnd" target="_blank">
-                    <IconButton>
-                        <Github size={24} />
-                    </IconButton>
-                </a>
-                <a href="#" target="_blank">
-                    <IconButton>
-                        <BookUser size={24} />
-                    </IconButton>
-                </a>
-            </nav>
+            <div className="container sm:px-8 md:px-9 lg:px-32 px-3 py-8 header--wrapper">
+                <h2 className="header--title">Meme Generator</h2>
+                <nav className="flex flex-row gap-1">
+                    <ThemeSwitcher />
+                    <a href="https://github.com/SamPwnd" target="_blank">
+                        <IconButton>
+                            <Github size={24} />
+                        </IconButton>
+                    </a>
+                    <a href="#" target="_blank">
+                        <IconButton>
+                            <BookUser size={24} />
+                        </IconButton>
+                    </a>
+                </nav>
+            </div>
+            
             
             
         </header>
